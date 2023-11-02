05b17cdba684584d2699e50fd109ababcb8b3c52
DatLag <jeff.retz08@gmail.com>
2023-11-02 20:19:18 +0100

    added maven-publish info

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `kotlin-dsl`
     kotlin("jvm")
@@ -19,11 +21,56 @@ dependencies {
 
 gradlePlugin {
     plugins {
+        website.set("https://github.com/DatL4g/Sekret")
+        vcsUrl.set("https://github.com/DatL4g/Sekret")
+
         create("sekretPlugin") {
             id = artifact
             implementationClass = "dev.datlag.sekret.gradle.SekretGradlePlugin"
             displayName = "Sekret Gradle Plugin"
             description = "Gradle Plugin for Sekret"
+            tags.set(listOf("kotlin", "secret", "hidden"))
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(host = SonatypeHost.S01, automaticRelease = true)
+    signAllPublications()
+
+    val publishId = "sekret-gradle-plugin"
+
+    coordinates(
+        groupId = artifact,
+        artifactId = publishId,
+        version = libVersion
+    )
+
+    pom {
+        name.set(publishId)
+        description.set("Deeply hide secrets with Kotlin Multiplatform")
+        url.set("https://github.com/DatL4g/Sekret")
+        inceptionYear.set("2023")
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/DatL4g/Sekret")
+            connection.set("scm:git:git://github.com/DATL4G/Sekret.git")
+            developerConnection.set("scm:git:git://github.com/DATL4G/Sekret.git")
+        }
+
+        developers {
+            developer {
+                id.set("DatL4g")
+                name.set("Jeff Retz")
+                url.set("https://github.com/DatL4g")
+            }
         }
     }
 }
