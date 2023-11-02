4aba28ec5789b431f155f44f3fc33865ce07bdad
DatLag <jeff.retz08@gmail.com>
2023-11-03 00:46:41 +0100

    added include sekret module hint

--- gradle-plugin/src/main/java/dev/datlag/sekret/gradle/SekretGradlePlugin.kt
+++ gradle-plugin/src/main/java/dev/datlag/sekret/gradle/SekretGradlePlugin.kt
@@ -5,6 +5,8 @@ import dev.datlag.sekret.gradle.helper.Utils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.initialization.Settings
+import org.gradle.api.internal.GradleInternal
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.create
@@ -68,6 +70,15 @@ class SekretGradlePlugin : Plugin<Project> {
             }
         }
 
+    private val Project.settings: Settings?
+        get() = (this.rootProject as? GradleInternal?)?.settings
+
+    private fun Project.applyToSettings(block: (Settings) -> Unit) {
+        this.settings?.let(block) ?: this.rootProject.gradle.beforeProject {
+            this.settings?.let(block)
+        }
+    }
+
     override fun apply(target: Project) {
         target.extensions.create(
             "sekret",
@@ -79,6 +90,14 @@ class SekretGradlePlugin : Plugin<Project> {
 
         val srcFolder = createSekretFolders(sekretDir)
         val generateTask = target.tasks.maybeCreate("generateSekret")
+
+        target.applyToSettings { settings ->
+            runCatching {
+                target.findProject("sekret")?.path
+            }.getOrNull()?.let {
+                settings.include(it)
+            }
+        }
 
         generateTask.doFirst {
             createNativeSourceFiles(target, srcFolder)
@@ -102,15 +121,35 @@ class SekretGradlePlugin : Plugin<Project> {
         when (target.kotlinExtension) {
             is KotlinSingleTargetExtension<*> -> {
                 target.dependencies {
-                    add("implementation", target.project("sekret"))
+                    runCatching {
+                        target.findProject("sekret")
+                    }.getOrNull()?.let {
+                        add("implementation", it)
+                    } ?: run {
+                        sekretModuleNotLoaded(
+                            target.path ?: String()
+                        )
+                    }
                 }
             }
             is KotlinMultiplatformExtension -> {
                 target.dependencies {
-                    add("commonMainImplementation", target.project("sekret"))
+                    runCatching {
+                        target.findProject("sekret")
+                    }.getOrNull()?.let {
+                        add("commonMainImplementation", it)
+                    } ?: run {
+                        sekretModuleNotLoaded(
+                            target.path ?: String()
+                        )
+                    }
                 }
             }
         }
+    }
+
+    private fun sekretModuleNotLoaded(path: String) {
+        println("Seems like the ${path}${Project.PATH_SEPARATOR}sekret module isn't included to your settings, please add it")
     }
 
     private fun createSekretFolders(sekretDir: File): File {
--- sample/src/jvmMain/kotlin/Main.kt
+++ sample/src/jvmMain/kotlin/Main.kt
@@ -1,7 +1,3 @@
-import dev.datlag.sekret.sample.Sekret
-
 fun main() {
     println("Hello World")
-    val sekretClass = Sekret()
-    sekretClass.sampleKey("dev.datlag.sekret").let(::println)
 }
