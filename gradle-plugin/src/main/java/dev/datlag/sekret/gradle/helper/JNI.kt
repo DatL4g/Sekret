ac4ae31e4aa761905533a4d2b107039f4c07d3e2
DatLag <jeff.retz08@gmail.com>
2023-11-07 23:31:13 +0100

    fix generated files

--- gradle-plugin/src/main/java/dev/datlag/sekret/gradle/helper/JNI.kt
+++ gradle-plugin/src/main/java/dev/datlag/sekret/gradle/helper/JNI.kt
@@ -10,12 +10,18 @@ object JNI {
     private const val J_CHAR_VAR = "jcharVar"
     private const val J_CLASS = "jclass"
 
+    private const val LIBRARY_J_STRING = "jString"
+    private const val LIBRARY_J_OBJECT = "jObject"
+
     fun jniEnvVar(packageName: String) = ClassName(packageName, JNI_ENV_VAR)
     fun jString(packageName: String) = ClassName(packageName, J_STRING)
     fun jCharVar(packageName: String) = ClassName(packageName, J_CHAR_VAR)
     fun jClass(packageName: String) = ClassName(packageName, J_CLASS)
 
     fun getOriginalValue(packageName: String) = MemberName(packageName, "getOriginalValue")
+
+    fun libraryJString(packageName: String) = ClassName(packageName, LIBRARY_J_STRING)
+    fun libraryJObject(packageName: String) = ClassName(packageName, LIBRARY_J_OBJECT)
 
     sealed class Error(private val msg: String) {
         object NewString : Error("Could not find NewString method in JNI")
